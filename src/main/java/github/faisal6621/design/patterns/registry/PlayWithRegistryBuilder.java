335b56a9ead5578b3a614217754f2bb24ded03ad
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-03-31 23:45:52 +0530

    refactored registry's main class

--- src/main/java/github/faisal6621/design/patterns/registry/PlayWithRegistryBuilder.java
+++ src/main/java/github/faisal6621/design/patterns/registry/PlayWithRegistryBuilder.java
@@ -1,4 +1,4 @@
-package github.faisal6621.design.patterns.factory;
+package github.faisal6621.design.patterns.registry;
 
 import java.util.function.Consumer;
 
@@ -6,7 +6,7 @@ import github.faisal6621.bean.Rectangle;
 import github.faisal6621.bean.Shape;
 import github.faisal6621.bean.Triangle;
 import github.faisal6621.design.patterns.builder.Builder;
-import github.faisal6621.design.patterns.registry.Registry;
+import github.faisal6621.design.patterns.factory.FactorySupplier;
 import github.faisal6621.enums.ShapeEnum;
 
 /**
@@ -15,8 +15,8 @@ import github.faisal6621.enums.ShapeEnum;
 public class PlayWithRegistryBuilder {
 
     public static void main(String[] args) {
-        Consumer<Builder<Shape>> rectangleConsumer = (builder) -> builder.register(ShapeEnum.RECTANGLE, Rectangle::new);
-        Consumer<Builder<Shape>> triangleConsumer = (builder) -> builder.register(ShapeEnum.TRIANGLE, Triangle::new);
+        Consumer<Builder<Shape>> rectangleConsumer = builder -> builder.register(ShapeEnum.RECTANGLE, Rectangle::new);
+        Consumer<Builder<Shape>> triangleConsumer = builder -> builder.register(ShapeEnum.TRIANGLE, Triangle::new);
         Consumer<Builder<Shape>> initializer = rectangleConsumer.andThen(triangleConsumer);
 
         Registry registry = Registry.createRegistry(initializer, shape -> {
@@ -24,9 +24,10 @@ public class PlayWithRegistryBuilder {
         });
         FactorySupplier<Shape> rectangleFactory = registry.buildShapeFactory(ShapeEnum.RECTANGLE);
         FactorySupplier<Shape> triangleFactory = registry.buildShapeFactory(ShapeEnum.TRIANGLE);
-        FactorySupplier<Shape> squareFactory = registry.buildShapeFactory(ShapeEnum.SQUARE);
         System.out.println(rectangleFactory.newInstance());
         System.out.println(triangleFactory.newInstance());
+
+        FactorySupplier<Shape> squareFactory = registry.buildShapeFactory(ShapeEnum.SQUARE);
         System.out.println(squareFactory.newInstance());
     }
 }

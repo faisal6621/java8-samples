73070c2d1d82abf38f730a55c8bfe8389937b413
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-05-05 15:57:05 +0530

    new challenges

---
+++ src/main/java/github/faisal6621/other/StringChallenge.java
@@ -0,0 +1,21 @@
+package github.faisal6621.other;
+
+public class StringChallenge {
+
+    @SuppressWarnings({ "java:S4973", "java:S2129" })
+    public static void main(String[] args) {
+        String s = "ABCDE";
+        String s1 = s.substring(0, 2);
+        String s2 = s.substring(0, 2);
+        System.out.println(s1.equals(s2));
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+
+        String b1 = "ABC";
+        String b2 = new String("ABC");
+        System.out.println(b1.hashCode());
+        System.out.println(b2.hashCode());
+    }
+}
